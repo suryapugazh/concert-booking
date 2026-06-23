5490c47c59ce580ddb4fbe22eb8499abe0355ae8
Surya <suryapugazh27@gmail.com>
2026-06-23 17:11:37 +0530

    Implement Posthog

---
+++ instrumentation-client.ts
@@ -0,0 +1,7 @@
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2026-05-30',
+    debug: process.env.NODE_ENV === "development"
+})
